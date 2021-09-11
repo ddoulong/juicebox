a23d7655c7989a656accb65207a6d5579f608f0d
DanRD <danid92@vt.edu>
2021-09-11 12:03:43 -0400

    past installing morgan, updating routes.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const PORT = 3000;
 const express = require('express');
 const server = express();
+const apiRouter = require('./api');
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
 
 server.use((req, res, next) => {
     console.log("<____Body Logger START____>");
@@ -9,6 +16,8 @@ server.use((req, res, next) => {
   
     next();
   });
+console.log("me");
+server.use('/api', apiRouter);
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
